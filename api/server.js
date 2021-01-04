907bb87bac86823e96baa98cb3e61c5f34325b65
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-04 14:15:16 -0800

    added the users route

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const server = express();
+const userRouter = require('../api/users/user-router');
 
 server.use(express.json());
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
     res.json({message: "Water My Plants"});
