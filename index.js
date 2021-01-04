e1f96d3516eb2bfaecd0d3ea35fd1d05b277a528
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-04 10:42:30 -0800

    server running

---
+++ index.js
@@ -0,0 +1,10 @@
+const server = require('./api/server');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
+});
