ed8b67bb24f75095052bc177ebf031943ce272b8
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-05 10:24:25 -0800

    created plants table and changed naming in users seed data

--- seeds/001-users.js
+++ seeds/001-users.js
@@ -1,11 +1,9 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
+exports.seed = function (knex) {
   return knex('users').truncate()
     .then(function () {
-      // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'josephv_12', password: 'password', phone_number: '626-290-2222' },
+        { id: 1, username: 'josephv_12', password: 'password', phone_number: '626-290-2222' },
       ]);
     });
 };
