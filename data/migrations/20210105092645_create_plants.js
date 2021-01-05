964cd71b58309176b1efacda22bdcfeeedd68be1
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-05 09:35:35 -0800

    created the plants table

---
+++ data/migrations/20210105092645_create_plants.js
@@ -0,0 +1,17 @@
+
+exports.up = function (knex) {
+    return knex.schema
+    .createTable('plants', table => {
+        table.increments();
+        table.string('nickname', 129).notNullable();
+        table.string('species', 129).notNullable();
+        table.string('h2o_frequency', 129).notNullable();
+        table.string('plant_image').notNullable();
+        table.string('group', 129).notNullable();
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema
+    .dropTableIfExists('plants');
+};
