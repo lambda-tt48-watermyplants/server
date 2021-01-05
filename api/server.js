44f1ae1bf0a6e9e79b6ea6293b901061e89fd235
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-05 10:43:00 -0800

    added the plants router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const server = express();
+
 const userRouter = require('../api/users/user-router');
+const plantsRouter = require('../api/plants/plant_router');
 
 server.use(express.json());
 server.use('/api/users', userRouter);
+server.use('/api/plants', plantsRouter);
 
 server.get('/', (req, res) => {
     res.json({message: "Water My Plants"});
