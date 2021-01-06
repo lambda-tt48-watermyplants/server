8c3a9de095604c2816dbeaa12617871cade77dfa
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-06 14:46:20 -0800

    removed create_plants migration and moved it into create_users and added a foreign key referencing to the user who posted post

--- data/migrations/20210104150211_create_users.js
+++ data/migrations/20210104150211_create_users.js
@@ -2,11 +2,26 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable('users', table => {
-            table.increments();
+            table.increments('user_id');
             table.string('username', 128).notNullable();
             table.string('password', 128).notNullable();
             table.string('phone_number', 128).unique().notNullable();       
         })
+        .createTable('plants', table => {
+            table.increments('plant_id');
+            table.string('nickname', 129).notNullable();
+            table.string('species', 129).notNullable();
+            table.integer('h2oFrequency');
+            table.string('plantImage').notNullable();
+            table.string('group', 129).notNullable();
+            table.string('instructions', 129).notNullable();
+            table.string('lastWater', 129).notNullable();
+            table.string('nextWater', 129).notNullable();
+            table.integer('id_user')
+            .unsigned()
+            .references('user_id').inTable('users')
+            .onDelete('RESTRICT').onUpdate('RESTRICT')
+        });
 };
 
 exports.down = function (knex) {
--- seeds/001-plants.js
+++ seeds/001-plants.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('plants').insert([
-        {id: 1, nickname: 'Parlor Palm', species: 'Chamaedorea elegans', plantImage: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_parlor-palm_variant_small_grant_black_b4347b61-c7ce-4d59-ac07-bcf45e773f4a_1200x.jpg?v=1608586313', group: 'Chamaedorea', instructions: 'Water every 1-2 weeks', lastWater: '', nextWater: ''   },
+        {plant_id: 1, nickname: 'Parlor Palm', species: 'Chamaedorea elegans', plantImage: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_parlor-palm_variant_small_grant_black_b4347b61-c7ce-4d59-ac07-bcf45e773f4a_1200x.jpg?v=1608586313', group: 'Chamaedorea', instructions: 'Water every 1-2 weeks', lastWater: '', nextWater: '', id_user: 1},
       ]);
     });
 };
