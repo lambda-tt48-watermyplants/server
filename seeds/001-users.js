a07911134934731f324a773202f207d80cf32464
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-06 12:46:05 -0800

    removed users seed and update name for plants seed

--- seeds/001-users.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = function (knex) {
-  return knex('users').truncate()
-    .then(function () {
-      return knex('users').insert([
-        { id: 1, username: 'josephv_12', password: 'password', phone_number: '626-290-2222' },
-      ]);
-    });
-};
