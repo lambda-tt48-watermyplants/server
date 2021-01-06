5e8d4bf1f2390b254ef9aec519fdf419863bc57f
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-01-06 12:36:15 -0800

    updated seed data and migrations

--- data/migrations/20210105092645_create_plants.js
+++ data/migrations/20210105092645_create_plants.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
             table.increments();
             table.string('nickname', 129).notNullable();
             table.string('species', 129).notNullable();
-            table.string('h2oFrequency', 129).notNullable();
+            table.integer('h2oFrequency');
             table.string('plantImage').notNullable();
             table.string('group', 129).notNullable();
             table.string('instructions', 129).notNullable();
